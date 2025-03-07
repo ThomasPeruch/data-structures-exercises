a638c6d2feb27facae79a600757f534a58f28c87
Thomas Peruch <peruchthomas@gmail.com>
2025-03-06 22:27:07 -0300

    Queue - with exercise

--- main.py
+++ main.py
@@ -1,44 +1,50 @@
-class Stack:
+class Queue:
     def __init__(self):
         self.itens = []
 
-    def push(self,item):
+    def enqueue(self, item):
         self.itens.append(item)
 
-    def pop(self):
-        if len(self.itens) > 0:
-            return self.itens.pop()
-        else :
-            return 'Lista Vazia'
-    
+    def dequeue(self):
+        if not self.is_empty():
+            return self.itens.pop(0) 
+        return 'Fila vazia'
+
     def peek(self):
-        if len(self.itens) > 0:
+        if not self.is_empty():
             return self.itens[-1]
-        else:
-            return 'Lista Vazia'
+        return 'Fila vazia'
 
-def verifica_balanceamento(expressao):
-    stack = Stack()
+    def is_empty(self):
+        return len(self.itens) == 0
 
-    for char in expressao:
-        if char == "(":
-           stack.push(char)
-        elif char == ")":
-            if len(stack.itens) == 0:
-                return False
-            stack.pop()
-    return len(stack.itens) == 0
+def printing(documents):
+    queue = Queue()
 
-expressoes = [
-    "(a + b) * (c + d)", 
-    "(a + b * (c + d)",
-    "((a + b) * c)",
-    "((a + b)) * (c + d))",
-    "(a + b)",
-    "a + b * c - d",
+    priorities_values = {'Low': 5, 'Medium': 3, 'High': 1}
+
+    print('Receiving documents...\n')
+
+    for document in documents:
+        queue.enqueue(document)
+
+    while not queue.is_empty() :
+        doc = queue.dequeue()
+
+        for i in range(priorities_values[doc['priority']], 0, -1):
+            print(f'printing in {i}')
+        print(f"{doc['name']} printed\n")
+
+    print("Printer's Queue is Empty")
+
+documents = [
+    {'name': 'Annual Sales Report 2024','priority': 'Medium'},
+    {'name': 'Payroll 25/01', 'priority':'High'}, 
+    {'name': 'New Vendor Presentation', 'priority': 'Low'},
+    {'name': 'Internship Program Presentation', 'priority': 'Medium'},
+    {'name': 'Data Traffic in Marketplace 2024 Q4','priority': 'Medium'},
+    {'name': 'Stock Flutuation','priority': 'Medium'},
+    {'name': 'Vacation Document', 'priority': 'High'}
 ]
 
-for expressao in expressoes:
-    print(f"'{expressao}': {verifica_balanceamento(expressao)}")
-        
-
+printing(documents)
