a0dbc0e633db072daf16f4038def2dcd7ec414cc
Thomas Peruch <peruchthomas@gmail.com>
2025-03-06 21:12:05 -0300

    exercise with stack

--- main.py
+++ main.py
@@ -3,18 +3,42 @@ class Stack:
         self.itens = []
 
     def push(self,item):
-        self.append(item)
+        self.itens.append(item)
 
     def pop(self):
-        if not len(self.itens) > 0:
+        if len(self.itens) > 0:
             return self.itens.pop()
         else :
             return 'Lista Vazia'
     
-    def peek(self, item):
-        if not self.itens.isEmpty():
+    def peek(self):
+        if len(self.itens) > 0:
             return self.itens[-1]
         else:
             return 'Lista Vazia'
 
-stack = Stack()
+def verifica_balanceamento(expressao):
+    stack = Stack()
+
+    for char in expressao:
+        if char == "(":
+           stack.push(char)
+        elif char == ")":
+            if len(stack.itens) == 0:
+                return False
+            stack.pop()
+    return len(stack.itens) == 0
+
+expressoes = [
+    "(a + b) * (c + d)", 
+    "(a + b * (c + d)",
+    "((a + b) * c)",
+    "((a + b)) * (c + d))",
+    "(a + b)",
+    "a + b * c - d",
+]
+
+for expressao in expressoes:
+    print(f"'{expressao}': {verifica_balanceamento(expressao)}")
+        
+
